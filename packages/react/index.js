b6cde1cbb4ca8054731419d7c3f121b906c6b89f
Bayathy <misosoup0321@icloud.com>
2023-05-20 22:39:41 +0900

    feat: create react config

---
+++ packages/react/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:jsx-a11y/recommended",
+  ],
+  rules: {
+    "jsx-a11y/aria-props": "warn",
+    "jsx-a11y/aria-proptypes": "warn",
+    "jsx-a11y/aria-unsupported-elements": "warn",
+    "jsx-a11y/role-has-required-aria-props": "warn",
+    "jsx-a11y/role-supports-aria-props": "warn",
+  },
+};
